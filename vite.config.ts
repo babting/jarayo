2e3cc34d199175b623871875d2ca6b6284628ebb
babting <70562071+babting@users.noreply.github.com>
2026-02-03 09:14:17 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    // TODO: 만약 GitHub 저장소 이름이 'omniscient-baby-view'가 아니라면 아래 값을 실제 저장소 이름으로 변경해주세요.
-    // 예: base: '/my-repo-name/'
-    base: '/jarayo/',
-    build: {
-      outDir: 'dist'
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/jarayo/', // 이 부분이 있어야 배포 후 화면이 보입니다!
+})
